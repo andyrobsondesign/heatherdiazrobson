e2a864ace48743eb261427aaea44405401bd94c4
Bobbi Mullins <Mullins@Bobbis-MacBook-Pro.local>
2016-06-23 21:58:19 -0400

    changes

---
+++ Sites/lisa_robson/wp-content/themes/accelerate2-theme-child/about-page.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: about-page
+*/
+?>
+<?php
+/**
+ * The template for displaying the About Page
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+get_header(); ?>
+
+
+<?php get_footer(); ?>
